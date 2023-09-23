cf5a3864f2eca1ef84870f50fddbcdbaffdf87ac
A-Abd <abrar.abdulwahed@gmail.com>
2023-09-23 23:57:33 +0300

    fix: try with cosutom hook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from './Button'
 
 function Footer ({ toggleModal }: any) {
-//   const [value, toggleValue] = useToggle(false)
+  const [value, toggleValue] = useToggle(false)
   return (
     <footer className='footer'>
         <Button name="Rules" classes='btn-outline' handleClick={toggleModal} />
